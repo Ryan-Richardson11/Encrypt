36e3bab1e2473dfbbd4d89ecccd4ae4da4f1a284
Ryan Richardson <ryanrich811@gmail.com>
2025-01-14 13:59:30 -0500

    Implemented RSA functions

--- src/encryption/encryption_functions.py
+++ src/encryption/encryption_functions.py
@@ -1,4 +1,4 @@
-from Crypto.Cipher import AES, DES, DES3
+from Crypto.Cipher import AES, DES, DES3, PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 
@@ -145,38 +145,49 @@ class Encrypt:
 # =========================================
 #                   RSA
 # =========================================
-    def encrypt_3DES(self, file_name):
-        des3_key = get_random_bytes(24)
-        nonce = get_random_bytes(4)
+    def generate_RSA_Key(self):
+        key = RSA.generate(2048)
+        private = key.export_key()
+        public = key.publickey().export_key()
 
-        encryption = DES3.new(des3_key, DES3.MODE_CTR, nonce=nonce)
+        with open("private.pem", "wb") as private_key_file:
+            private_key_file.write(private)
+
+        with open("public.pem", "wb") as public_key_file:
+            public_key_file.write(public)
+
+    def encrypt_RSA(self, file_name, public_key_file="public.pem"):
+        # Read the public key from file
+        with open(public_key_file, "rb") as pub_key_file:
+            public_key = RSA.import_key(pub_key_file.read())
+
+        # Create an RSA cipher using OAEP padding (secure padding scheme)
+        cipher_rsa = PKCS1_OAEP.new(public_key)
 
         # Read the file and encrypt it
-        with open(f'{file_name}', "rb") as input_file:
+        with open(file_name, "rb") as input_file:
             data = input_file.read()
-            encrypted = encryption.encrypt(data)
+            encrypted = cipher_rsa.encrypt(data)
 
-        # Save the encrypted file with nonce and 3DES key at the beginning
+        # Save the encrypted file
         encrypted_file = file_name + ".enc"
         with open(encrypted_file, "wb") as encrypted_file:
-            encrypted_file.write(nonce)
-            encrypted_file.write(des3_key)
             encrypted_file.write(encrypted)
 
-        print(f"File encrypted and saved as {encrypted_file}")
+        print(f"File encrypted and saved as {encrypted_file.name}")
 
-    def decrypt_3DES(self, encrypted_file_name):
-        # Open the encrypted file and read the nonce, 3DES key, and encrypted data
+    def decrypt_RSA(self, encrypted_file_name, private_key_file="private.pem"):
+        # Read the private key from file
+        with open(private_key_file, "rb") as priv_key_file:
+            private_key = RSA.import_key(priv_key_file.read())
+
+        # Create an RSA cipher using OAEP padding (same padding as for encryption)
+        cipher_rsa = PKCS1_OAEP.new(private_key)
+
+        # Read the encrypted file and decrypt it
         with open(encrypted_file_name, "rb") as encrypted_file:
-            nonce = encrypted_file.read(4)
-            des3_key = encrypted_file.read(24)
             encrypted_data = encrypted_file.read()
-
-        # Create 3DES cipher in CTR mode with the same nonce and key
-        encryption = DES3.new(des3_key, DES3.MODE_CTR, nonce=nonce)
-
-        # Decrypt the data
-        decrypted_data = encryption.decrypt(encrypted_data)
+            decrypted_data = cipher_rsa.decrypt(encrypted_data)
 
         # Replace ".enc" in the file name to create the decrypted file name
         decrypted_file_name = encrypted_file_name.replace(".enc", "_decrypted")
